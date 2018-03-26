88c4aaf01f32c1aaf1753d98eabb3bea0297ac3b
Zhe Jiang <z.jiang.te@gmail.com>
2018-03-25 22:27:41 -0700

    SQL scripts added in dir

---
+++ DB Scripts/insert into volunteerShifts.sql
@@ -0,0 +1,64 @@
+insert into volunteerShift
+values('1','2016-6-6', ‘yes’, ‘dismantle’, ‘00030’, ‘2016-6-1’, ‘yes’, ‘00010’);
+insert into volunteerShift
+values(‘2’, ‘2016-7-19’, ‘yes’, ‘RAM testing’, ‘00030’, ‘2016-7-10’, ‘yes’, ‘00009’);
+insert into volunteerShift
+values(‘1’, ‘2016-11-5’, ‘no’, ‘RAM testing’, ‘00030’, ‘2016-10-10’, ‘no’, null);
+insert into volunteerShift
+values(‘2’, ‘2017-1-5’, ‘no’, ‘cleaning’, ‘00030’, ‘2017-1-9’, ‘no’, null);
+insert into volunteerShift
+values(‘2’, ‘2017-6-5’, ‘no’, ‘dismantle’, ‘00030’, ‘2017-6-21’, ‘no’, null);
+insert into volunteerShift
+values(‘1’, ‘2017-8-15’, ‘no’, ‘dismantle’, ‘00030’, ‘2017-8-22’, ‘no’, null);
+insert into volunteerShift
+values(‘2’, ‘2017-9-12’, ‘no’, ‘cleaning’, ‘00030’, ‘2017-10-10’, ‘no’, null);
+insert into volunteerShift
+values(‘2’, ‘2017-11-5’, ‘yes’, ‘hard drive install’, ‘00030’, ‘2017-10-10’, ‘yes’, ‘00010’);
+insert into volunteerShift
+values(‘2’, ‘2018-1-6’, ‘no’, ‘hard drive install’, ‘00030’, ‘2018-1-30’,‘no’, null);
+insert into volunteerShift
+values(‘1’, ‘2018-3-14’, ‘no’, ‘RAM testing’, ‘00030’, ‘2018-3-20’,‘no’, null);
+insert into volunteerShift
+values(‘2’, ‘2018-8-5’, ‘no’, ‘dismantle’, ‘00030’, ‘2018-1-10’,‘no’, null);
+insert into volunteerShift
+values(‘1’, ‘2018-5-6’, ‘no’, ‘hard drive install’, ‘00030’, ‘2016-3-10’,‘no’, null);
+insert into volunteerShift
+values(‘2’, ‘2018-4-20’, ‘no’, ‘cleaning’, ‘00030’, ‘2018-2-22’,‘no’, null);
+
+insert into volunteerShift
+values('1','2015-7-16', ‘no’, ‘dismantle’, ‘00031’, ‘2015-7-1’, ‘yes’, ‘00010’);
+insert into volunteerShift
+values('1','2015-12-1', ‘no’, ‘RAM testing’, ‘00031’, ‘2015-11-13’, ‘yes’, ‘00010’);
+insert into volunteerShift
+values('1','2016-1-7', ‘no’, ‘dismantle’, ‘00031’, ‘2015-12-1’, ‘no’, ‘null’);
+insert into volunteerShift
+values('2','2016-3-4', ‘yes’, ‘dismantle’, ‘00031’, ‘2016-1-1’, ‘no, null);
+
+
+insert into volunteerShift
+values(‘2’, ‘2016-7-19’, ‘yes’, ‘RAM testing’, ‘00030’, ‘2016-7-10’, ‘00009’);
+insert into volunteerShift
+values(‘1’, ‘2016-11-5’, ‘no’, ‘RAM testing’, ‘00030’, ‘2016-10-10’, null);
+insert into volunteerShift
+values(‘2’, ‘2017-1-5’, ‘no’, ‘cleaning’, ‘00030’, ‘2017-1-9’, null);
+insert into volunteerShift
+values(‘2’, ‘2017-6-5’, ‘no’, ‘dismantle’, ‘00030’, ‘2017-6-21’, null);
+insert into volunteerShift
+values(‘1’, ‘2017-8-15’, ‘no’, ‘dismantle’, ‘00030’, ‘2017-8-22’, null);
+insert into volunteerShift
+values(‘2’, ‘2017-9-12’, ‘no’, ‘cleaning’, ‘00030’, ‘2017-10-10’, null);
+insert into volunteerShift
+values(‘2’, ‘2017-11-5’, ‘yes’, ‘hard drive install’, ‘00030’, ‘2017-10-10’, ‘00010’);
+insert into volunteerShift
+values(‘2’, ‘2018-1-6’, ‘no’, ‘hard drive install’, ‘00030’, ‘2018-1-30’, null);
+insert into volunteerShift
+values(‘1’, ‘2018-3-14’, ‘no’, ‘RAM testing’, ‘00030’, ‘2018-3-20’, null);
+insert into volunteerShift
+values(‘2’, ‘2018-8-5’, ‘no’, ‘dismantle’, ‘00030’, ‘2018-1-10’, null);
+insert into volunteerShift
+values(‘1’, ‘2018-5-6’, ‘no’, ‘hard drive install’, ‘00030’, ‘2016-3-10’, null);
+insert into volunteerShift
+values(‘2’, ‘2018-4-20’, ‘no’, ‘cleaning’, ‘00030’, ‘2018-2-22’, null);
+    
+
+
