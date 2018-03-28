1692665275b9d7986ffccf60dc0e8823a5eb783c
anita76 <o2t0b@ugrad.cs.ubc.ca>
2018-03-27 22:15:07 -0700

    fix merge issue

--- src/JDBC/Employee.java
+++ src/JDBC/Employee.java
@@ -14,7 +14,6 @@ public class Employee extends GuestUser {
     private int supID;
     public Employee(int ID)throws SQLException {
         super(ID);
-        sb.append("select  from users where id=");
     }
 
     // select a day and return all free start time blocks(1 hour duration)
