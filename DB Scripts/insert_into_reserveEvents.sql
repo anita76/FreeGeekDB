2a2013aaf9f26016ccddf9444dedbd4e91cb6fff
panzerfist <jimmy.zjiang@gmail.com>
2018-03-28 09:38:59 -0700

    insertion of reserveEventsn added

---
+++ DB Scripts/insert_into_reserveEvents.sql
@@ -0,0 +1,191 @@
+insert into reserveEvents
+	values('2013-06-30', '18:00:00', '2013-06-01', '00001');
+
+insert into reserveEvents
+	values('2013-06-30', '18:00:00', '2013-06-01', '00002');
+
+insert into reserveEvents
+	values('2013-06-30', '18:00:00', '2013-06-05', '00003');
+
+insert into reserveEvents
+	values('2013-06-30', '18:00:00', '2013-06-05', '00004');
+
+insert into reserveEvents
+	values('2013-06-30', '18:00:00', '2013-06-05', '00005');
+
+insert into reserveEvents
+	values('2013-06-30', '18:00:00', '2013-06-05', '00006');
+
+insert into reserveEvents
+	values('2013-06-30', '18:00:00', '2013-06-05', '00007');
+
+insert into reserveEvents
+	values('2013-06-30', '18:00:00', '2013-06-06', '00008');
+
+insert into reserveEvents
+	values('2013-06-30', '18:00:00', '2013-06-05', '00009');
+
+insert into reserveEvents
+	values('2013-06-30', '18:00:00', '2013-06-05', '00010');
+
+
+insert into reserveEvents
+	values('2013-12-03', '18:00:00', '2013-09-25', '00011');
+
+insert into reserveEvents
+	values('2013-12-03', '18:00:00', '2013-10-25', '00012');
+
+insert into reserveEvents
+	values('2013-12-03', '18:00:00', '2013-10-25', '00013');
+
+insert into reserveEvents
+	values('2013-12-03', '18:00:00', '2013-10-25', '00014');
+
+
+insert into reserveEvents
+	values('2014-02-03', '18:00:00', '2014-01-01', '00021');
+
+insert into reserveEvents
+	values('2014-02-03', '18:00:00', '2014-01-02', '00022');
+
+insert into reserveEvents
+	values('2014-02-03', '18:00:00', '2014-01-03', '00023');
+
+insert into reserveEvents
+	values('2014-02-03', '18:00:00', '2014-01-03', '00024');
+
+insert into reserveEvents
+	values('2014-02-03', '18:00:00', '2014-01-01', '00025');
+
+insert into reserveEvents
+	values('2014-02-03', '18:00:00', '2014-01-01', '00026');
+
+insert into reserveEvents
+	values('2014-02-03', '18:00:00', '2014-01-01', '00027');
+
+insert into reserveEvents
+	values('2014-02-03', '18:00:00', '2014-01-03', '00028');
+
+insert into reserveEvents
+	values('2014-02-03', '18:00:00', '2014-01-01', '00029');
+
+insert into reserveEvents
+	values('2014-02-03', '18:00:00', '2014-01-03', '00030');
+
+insert into reserveEvents
+	values('2014-02-03', '18:00:00', '2014-01-03', '00031');
+
+
+insert into reserveEvents
+	values('2014-07-30', '18:00:00', '2014-05-21', '00035');
+
+insert into reserveEvents
+	values('2014-07-30', '18:00:00', '2014-05-21', '00036');
+
+insert into reserveEvents
+	values('2014-07-30', '18:00:00', '2014-05-21', '00037');
+
+
+insert into reserveEvents
+	values('2014-11-30', '18:00:00', '2014-10-21', '00040');
+
+insert into reserveEvents
+	values('2014-11-30', '18:00:00', '2014-10-21', '00041');
+
+insert into reserveEvents
+	values('2014-11-30', '18:00:00', '2014-10-21', '00042');
+
+insert into reserveEvents
+	values('2014-11-30', '18:00:00', '2014-10-21', '00043');
+
+
+insert into reserveEvents
+	values('2015-03-30', '18:00:00', '2015-02-09', '00046');
+
+insert into reserveEvents
+	values('2015-03-30', '18:00:00', '2015-02-09', '00047');
+
+insert into reserveEvents
+	values('2015-03-30', '18:00:00', '2015-02-09', '00048');
+
+
+insert into reserveEvents
+	values('2015-07-30', '18:00:00', '2015-06-19', '00050');
+
+insert into reserveEvents
+	values('2015-07-30', '18:00:00', '2015-06-19', '00051');
+
+insert into reserveEvents
+	values('2015-07-30', '18:00:00', '2015-06-19', '00052');
+
+insert into reserveEvents
+	values('2015-07-30', '18:00:00', '2015-06-19', '00053');
+
+insert into reserveEvents
+	values('2015-07-30', '18:00:00', '2015-06-19', '00054');
+
+insert into reserveEvents
+	values('2015-07-30', '18:00:00', '2015-06-19', '00055');
+
+insert into reserveEvents
+	values('2015-07-30', '18:00:00', '2015-06-19', '00056');
+
+
+insert into reserveEvents
+	values('2015-11-30', '18:00:00', '2015-10-29', '00060');
+
+insert into reserveEvents
+	values('2015-11-30', '18:00:00', '2015-10-29', '00061');
+
+insert into reserveEvents
+	values('2015-11-30', '18:00:00', '2015-09-29', '00062');
+
+insert into reserveEvents
+	values('2015-11-30', '18:00:00', '2015-10-29', '00063');
+
+insert into reserveEvents
+	values('2015-11-30', '18:00:00', '2015-10-29', '00064');
+
+insert into reserveEvents
+	values('2015-11-30', '18:00:00', '2015-10-21', '00065');
+
+insert into reserveEvents
+	values('2015-11-30', '18:00:00', '2015-10-29', '00066');
+
+insert into reserveEvents
+	values('2015-11-30', '18:00:00', '2015-10-24', '00067');
+
+insert into reserveEvents
+	values('2015-11-30', '18:00:00', '2015-10-29', '00068');
+
+insert into reserveEvents
+	values('2015-11-30', '18:00:00', '2015-10-25', '00069');
+
+insert into reserveEvents
+	values('2015-11-30', '18:00:00', '2015-10-29', '00070');
+
+insert into reserveEvents
+	values('2015-11-30', '18:00:00', '2015-10-24', '00071');
+
+insert into reserveEvents
+	values('2015-11-30', '18:00:00', '2015-10-29', '00072');
+
+insert into reserveEvents
+	values('2015-11-30', '18:00:00', '2015-10-29', '00073');
+
+
+insert into reserveEvents
+	values('2016-01-30', '18:00:00', '2016-01-03', '00080');
+
+insert into reserveEvents
+	values('2016-01-30', '18:00:00', '2016-01-03', '00081');
+
+
+insert into reserveEvents
+	values('2016-07-15', '18:00:00', '2016-06-16', '00086');
+
+insert into reserveEvents
+	values('2016-07-15', '18:00:00', '2016-06-16', '00087');
+
+insert into reserveEvents
+	values('2016-07-15', '18:00:00', '2016-06-16', '00088');
