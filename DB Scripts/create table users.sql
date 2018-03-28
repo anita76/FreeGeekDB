408d602edae8311855af0913c475cf9f04b4c44e
anita76 <o2t0b@ugrad.cs.ubc.ca>
2018-03-28 02:37:03 -0700

    updated db scripts with check constraint

--- DB Scripts/create table users.sql
+++
@@ -1,9 +0,0 @@
-create table users (
-    id  number(5) not null,
-    firstName char(30) not null,
-    lastName char(30) not null,
-    email char(50) unique,
-    phone char(20),
-    membershipStartData char(20),
-    primary key (id)
-);
